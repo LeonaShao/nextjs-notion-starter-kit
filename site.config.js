786dbbb48e442095d19a143d2a97fa676fb59bf4
LeonaShao <lianghua.shao@gmail.com>
2022-04-05 22:35:00 -0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,23 +1,23 @@
 export default {
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '108ed199926141179697db90e5903cb3',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '海上的阿提',
+  domain: 'https://www.notion.so/108ed199926141179697db90e5903cb3',
+  author: '海上的阿提',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '',
+  github: '',
+  linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
